1fb183732aecd0c948d7a803eb043e7dd9d748f5
Sergio Regar <117889913+SRGio7@users.noreply.github.com>
2024-10-30 09:29:43 +0800

    Home, header, footer Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header  from "./component/child_1/Header";
+import Home  from "./component/child_1/Home";
+import Footer  from "./component/child_1/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
